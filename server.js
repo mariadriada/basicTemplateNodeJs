7c3e52229abb140fafd139a740d84b5df2b7680d
Maria <mariagiraldo4@gmail.com>
2016-01-27 15:25:26 -0500

    Correcting declaration of variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http	= require('http');
 var url		= require('url');
 
 function initServer(route, handle){
-	function onRequest(request, respose){
+	function onRequest(request, response){
 		var pathname = url.parse(request.url).parse(request.url).pathname;
 		console.log('request received...');
 
