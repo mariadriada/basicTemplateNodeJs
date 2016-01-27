3b157561459b75c5b3c8034a8d90524d60dcd70e
Maria <mariagiraldo4@gmail.com>
2016-01-27 15:07:06 -0500

    Setting urls with the request handler

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var server			= require("./server");
 var router			= require("./router");
 var requestHandler	= require("./requestHandler");
 
+var handle = {};
+handle["/"] = requestHandler.home;
+handle["/home"] = requestHandler.home;
+
 
 
 
