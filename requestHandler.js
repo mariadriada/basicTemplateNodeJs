2fa8bf16d45e0371070bd9717834c939b203cffd
Maria <mariagiraldo4@gmail.com>
2016-01-27 17:31:15 -0500

    Adding html page on body variable

--- requestHandler.js
+++ requestHandler.js
@@ -4,9 +4,21 @@ var querystring	= require("querystring");
 function home(response){
 	console.log('Handler to / ');
 
-	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("Handler to / ");
-	response.end();
+	var body = '<html>'+
+	    '<head>'+
+	    '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />'+
+	    '</head>'+
+	    '<body>'+
+	    '<form action="/data" method="post">'+
+	    '<textarea name="text" rows="20" cols="60"></textarea>'+
+	    '<input type="submit" value="Enviar texto" />'+
+	    '</form>'+
+	    '</body>'+
+	    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
 exports.home = home;
