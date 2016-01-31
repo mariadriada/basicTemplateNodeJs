3558c3fd2c713a662929979664d898e79becb8aa
EMILIO <EMILIO>
2016-01-31 18:37:26 -0500

    add route /data linked to data function of request handlers

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var requestHandler	= require("./requestHandler");
 var handle = {};
 handle["/"] = requestHandler.home;
 handle["/home"] = requestHandler.home;
+handle["/data"] = requestHandler.data;
 
 server.initServer(router.route, handle);
 console.log('main');
